d6bd2bbcca0b400f4b6c77a104380acd23ead1da
Ven <omnom150@gmail.com>
2023-10-11 16:04:07 +0800

    added script
    work on this later

---
+++ Week5/Assets/Scripts/JigglePhysics.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JigglePhysics : MonoBehaviour
+{
+    public float intensity, mass, stiffness, dampness;
+    private Mesh OriginalMesh, MeshClone;
+    private MeshRenderer Renderer;
+    //private GameObjectVertex[] GV;
+    private Vector3[] VertexArray;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    //work on this later
+}
